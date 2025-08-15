63dc6d60823bf50f3d511966877e63507b388482
Vaugusto <samlara273@gmail.com>
2025-08-15 15:45:34 -0300

    🎉 Initial commit

---
+++ src/main/java/br/com/alura/challenge/literalura/LiteraluraApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.challenge.literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/challenge/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
